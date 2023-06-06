7a05f599f06e99e4f838cfb697597d23beaa4af2
TalelJandoubi <talel.axis@gmail.com>
2023-06-06 10:34:39 +0200

    ajout index.js

---
+++ Back/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = require('./routers/index.js');
+
+// creation de l'application
+const app = express();
+
+// Body 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Application routers
+app.use(router);
+
+// Demarrage serveur
+const PORT = process.env.PORT || 5430;
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT} ...`)
+});
