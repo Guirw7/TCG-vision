f614126aa1ad3d77791fec381bace3f20f788226
kevin-sotomayor <kevin.sotomayor@outlook.fr>
2023-06-06 11:20:47 +0200

    header, background, position adjusted

--- Front/src/components/Yugioh/index.tsx
+++ Front/src/components/Yugioh/index.tsx
@@ -3,7 +3,9 @@ import './styles.scss';
 export default function Yugioh() {
   return(
     <div className='game-container'>
-      <p className="game-title">Yu-Gi-Oh Trading Card Game</p>
+      <div className="game-container-background">
+        <p className="game-title">Yu-Gi-Oh Trading Card Game</p>
+      </div>
     </div>
   )
 }
