80aceca82f079ddce1fb6c8fec7410d47f187307
Guirw7 <guillaume.barban27@gmail.com>
2023-06-09 13:38:43 +0200

    feat: added module pg with connexion client and userDataMapper for request SQL

---
+++ Back/App/datamappers/userDataMapper.js
@@ -0,0 +1,24 @@
+// On recupère le client de connexion
+const client = require('../db');
+
+// On construit notre object userDataMapper
+const userDataMapper = {
+
+  /*
+   * Requête SQL pour inserer un user dans la base de données lors de l'inscription au formulaire
+   * en utilisant une requête préparer
+   */
+  async addUserInDB(user) {
+    const preparedQuery = {
+      text: 'INSERT INTO "user"(email, username, password) VALUES ($1, $2, $3) RETURNING *',
+      values: [user.email, user.username, user.password],
+    };
+
+    // On récupère le résultat de la requête préparer
+    const results = await client.query(preparedQuery);
+    return results.rows[0];
+  },
+};
+
+// On exporte le userDataMapper
+module.exports = userDataMapper;
