999fd5663e50120afb29e52e73054c6a351266dd
kevin-sotomayor <kevin.sotomayor@outlook.fr>
2023-06-09 22:08:41 +0200

    react-hook-form tests

--- Front/src/components/SignUp/index.tsx
+++ Front/src/components/SignUp/index.tsx
@@ -1,52 +1,78 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+// import CryptoJS from 'crypto-js';
+
 
 import './styles.scss';
 
+interface Data {
+  username: string;
+  mail: string; 
+  password: string;
+  passwordConfirmation: string;
+}
+
 export default function SignUp() {
-  const [username, setUsername] = useState<any>('');
-  const [email, setEmail] = useState<any>('');
-  const [emailConfirm, setEmailConfirm] = useState<any>('');
-  const [password, setPassword] = useState<any>('');
-  const [passwordConfirm, setPasswordConfirm] = useState<any>('');
+  
+  // const [username, setUsername] = useState<any>('');
+  // const [userEmail, setUserEmail] = useState<any>('');
+  // const [userEmailConfirm, setUserEmailConfirm] = useState<any>('');
 
-  const handleSubmit = (e: any) => {
-    console.log(
-      username.target.value, 
-      email.target.value, 
-      emailConfirm.target.value, 
-      password.target.value, 
-      passwordConfirm.target.value
-      );
-    e.preventDefault();
-  }
+  // On importe ici toutes les dépendances de react-hook-form:
+  const { 
+    register, 
+    handleSubmit, 
+    // watch, 
+    // clearErrors, 
+    formState: { errors } 
+  } = useForm<Data>({defaultValues : 
+  {
+    username: '',
+    mail: '',
+    password: '',
+    passwordConfirmation: ''
+  }});
+  // const onSubmit = (data: Data) => console.log(data);
+  console.log(errors);
+  // Permet de voir les inputs en temps réel
+  // console.log(watch("password"));
 
-  // Faire des vérifications sur les inputs :
-
-  // Vérifier que le username soit valide
-
-  // Vérifier que l'email soit valide
-  // Vérifier que les deux emails soient identiques
-  // Vérifier que les deux mots de passe soient identiques
-
-  // Vérifier que le mot de passe contienne au moins 8 caractères
-  // Vérifier que le mot de passe contienne au moins une majuscule
-  // Vérifier que le mot de passe contienne au moins un chiffre
-  // Vérifier que le mot de passe contienne au moins un caractère spécial
-
-  return(
+  return (
     <div className='signup-container'>
       <div className='signup-container-background'>
-        <h1 className='page-title'>Se connecter</h1>
-        <form className="signup-form" action="">
-          <input className="signup-input-username" type="text" onChange={setUsername} placeholder="Username"/>
-          <input className="signup-input-email" type="email" onChange={setEmail} placeholder="Email"/>
-          <input className="signup-input-email-confirm" type="email" onChange={setEmailConfirm} placeholder="Confirm Email" />
-          <input className="signup-input-password" type="password" onChange={setPassword} placeholder="Password"/>
-          <input className="signup-input-password-confirm" type="password" onChange={setPasswordConfirm} placeholder="Confirm Password" />
-          <button className="signup-input-button" type="submit" onClick={handleSubmit}>Sign Up</button>
-        </form>
+        <h1 className='page-title'>Création de compte</h1>
+          <form className="signup-form" onSubmit={handleSubmit((data) => {console.log(data)})}>
+            <input autoComplete='username' className="signup-input-username" type="text" placeholder="Nom d'utilisateur" 
+              {...register(
+                "username", 
+                {required: 'Ce champ est obligatoire', 
+                minLength: 4, 
+                maxLength: 16
+                })} />
+            <p className='test'>{errors.username?.message}</p>
+            <input autoComplete='email' className="signup-input-email" type="email" placeholder="Adresse mail" 
+              {...register(
+                "mail", 
+                {required: 'Ce champ est obligatoire', 
+                  maxLength: 64, 
+                  pattern: /^\S+@\S+$/i
+                })} />
+            <input autoComplete='off' className="signup-input-password" type="password" placeholder="Mot de passe"
+            {...register(
+              "password", 
+              {required: 'Ce champ est obligatoire', 
+              maxLength: 32, 
+              pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,32}$/i
+              })} 
+            />
+            <input autoComplete='off' className="signup-input-password-confirm" type="password" placeholder="Confirmez votre mot de passe" 
+              {...register("passwordConfirmation", 
+              {maxLength: 32, 
+              pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,32}$/i
+              })} />
+            <input className="signup-input-button" type="submit" />
+          </form>
+        </div>
       </div>
-    </div>
-
-  )
+  );
 } 
