e42f78b43817c1b1ebc59ff0d4b788bdc4a454ee
kevin-sotomayor <kevin.sotomayor@outlook.fr>
2023-06-10 16:55:31 +0200

    form done, only missing hash password and link with backend

--- Front/src/components/SignUp/index.tsx
+++ Front/src/components/SignUp/index.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 // import CryptoJS from 'crypto-js';
-
 
 import './styles.scss';
 
@@ -13,27 +11,28 @@ interface Data {
 }
 
 export default function SignUp() {
-  
-  // const [username, setUsername] = useState<any>('');
-  // const [userEmail, setUserEmail] = useState<any>('');
-  // const [userEmailConfirm, setUserEmailConfirm] = useState<any>('');
-
   // On importe ici toutes les dépendances de react-hook-form:
   const { 
     register, 
-    handleSubmit, 
+    handleSubmit,
+    // permet de générer des erreurs personnalisées
+    setError,
     // watch, 
     // clearErrors, 
     formState: { errors } 
-  } = useForm<Data>({defaultValues : 
-  {
-    username: '',
-    mail: '',
-    password: '',
-    passwordConfirmation: ''
-  }});
-  // const onSubmit = (data: Data) => console.log(data);
-  console.log(errors);
+  } = useForm<Data>(
+    // {defaultValues : 
+    //   {
+    //     username: 'kevin',
+    //     mail: 'kk@k.com',
+    //     password: 'LolXd69240',
+    //     passwordConfirmation: 'LolXd69240',
+    //   },
+    // }
+  );
+
+  // Créé un objet contenant les erreurs, est vide s'il n'y a pas d'erreurs
+  // console.log(errors);
   // Permet de voir les inputs en temps réel
   // console.log(watch("password"));
 
@@ -41,36 +40,71 @@ export default function SignUp() {
     <div className='signup-container'>
       <div className='signup-container-background'>
         <h1 className='page-title'>Création de compte</h1>
-          <form className="signup-form" onSubmit={handleSubmit((data) => {console.log(data)})}>
+          {/* onSubmit gère la soumission du formulaire */}
+          <form className="signup-form" onSubmit={handleSubmit((data) => {
+            // On vérifie que les mots de passe correspondent :
+            if (data.password !== data.passwordConfirmation) {
+              setError('passwordConfirmation', {
+                type: 'custom',
+                message: 'Les mots de passe ne correspondent pas.',
+              });
+            }
+          })}
+            >
             <input autoComplete='username' className="signup-input-username" type="text" placeholder="Nom d'utilisateur" 
+              // Méthode de react-hook form:
               {...register(
                 "username", 
                 {required: 'Ce champ est obligatoire', 
-                minLength: 4, 
-                maxLength: 16
+                minLength: {
+                  value: 4, 
+                  message: 'Votre nom d\'utilisateur doit contenir entre 4 et 16 caractères.',
+                },
+                maxLength: {
+                  value: 16,
+                  message: 'Votre nom d\'utilisateur doit contenir entre 4 et 16 caractères.',
+                },
                 })} />
-            <p className='test'>{errors.username?.message}</p>
+            {/* Message d'erreur à placer à l'endroit désiré: */}
+            <p className='error-message'>{errors.username?.message}</p>
             <input autoComplete='email' className="signup-input-email" type="email" placeholder="Adresse mail" 
               {...register(
                 "mail", 
-                {required: 'Ce champ est obligatoire', 
+                {required: 'Ce champ est obligatoire.', 
                   maxLength: 64, 
-                  pattern: /^\S+@\S+$/i
+                  pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i,
                 })} />
+                <p className='error-message'>{errors.mail?.message}</p>
+                {errors.mail && errors.mail.type === 'pattern' && (
+                <p className='error-message'>Le format de l'adresse mail est incorrect.</p>)
+                }
             <input autoComplete='off' className="signup-input-password" type="password" placeholder="Mot de passe"
             {...register(
               "password", 
-              {required: 'Ce champ est obligatoire', 
-              maxLength: 32, 
+              {required: 'Ce champ est obligatoire.', 
+              minLength: {
+                value: 8,
+                message: 'Votre mot de passe doit contenir entre 8 et 32 caractères.',
+              },
+              maxLength: {
+                value: 32,
+                message: 'Votre mot de passe doit contenir entre 8 et 32 caractères.',
+              }, 
               pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,32}$/i
               })} 
             />
+            {errors.password && errors.password.type === 'required' && (
+              <p className='error-message'>Le mot de passe est obligatoire.</p>)
+            }
             <input autoComplete='off' className="signup-input-password-confirm" type="password" placeholder="Confirmez votre mot de passe" 
-              {...register("passwordConfirmation", 
-              {maxLength: 32, 
-              pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,32}$/i
-              })} />
-            <input className="signup-input-button" type="submit" />
+              {...register(
+                "passwordConfirmation", {
+                  required: 'Veuillez confirmer votre mot de passe.',
+                }
+              )}
+              />
+              {errors.passwordConfirmation && <p className='error-message'>{errors.passwordConfirmation.message}</p>}
+            <input className="signup-input-button" type="submit"/>
           </form>
         </div>
       </div>
