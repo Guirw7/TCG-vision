b524f8b1a3a4189864618cb0f505ce3e1d4fba8c
Guirw7 <guillaume.barban27@gmail.com>
2023-06-12 10:41:30 +0200

    feat: added comments

--- Back/App/controllers/userController.js
+++ Back/App/controllers/userController.js
@@ -1,21 +1,31 @@
+// On require le module bcrypt pour le hash du mot de passe
 const bcrypt = require('bcrypt');
+// On require notre userDataMapper qui contient nos requête SQL
 const userDataMapper = require('../datamappers/userDataMapper');
 
 const userController = {
 
+  /**
+   * Fonction pour ajouter un utilisateur en base de données
+   */
   async addUserForm(req, res) {
+    // On récupère les infos envoyer par l'utilisateur
     const { email, username, password } = req.body;
     const saltRounds = 10;
+    // On hash le mot de passe avant de le passer à notre objet user
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
+    // On créer un objet avec les infos que l'utilisateur à envoyer dans le formulaire
     const user = {
       email,
       username,
       password: passwordHash,
     };
 
+    // On créer une variable en utilisant la méthode addUserInDB en lui passant notre objet user
     const newUser = await userDataMapper.addUserInDB(user);
 
+    // On renvoie la réponse au format JSON avec un status 200 (OK)
     res.status(200).json(newUser);
   },
 };
--- Back/App/validations/validate.js
+++ Back/App/validations/validate.js
@@ -1,3 +1,10 @@
+/**
+ * Fonction qui renvoie la validation du schema Joi concernant l'inscripton
+ * Si il y a une erreur on récupère et on affiche toutes les erreurs à afficher.
+ * @param {Object} schema - validation de Joi
+ * @returns {function} - un middleware
+ */
+
 function validate(schema) {
   return (req, res, next) => {
     const { error } = schema.validate(req.body, {
