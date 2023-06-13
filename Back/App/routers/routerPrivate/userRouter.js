ac344c593446b07c0160788242fc93d7d47330bd
TalelJandoubi <talel.axis@gmail.com>
2023-06-13 10:47:11 +0200

    added router validate for userRouter

--- Back/App/routers/routerPrivate/userRouter.js
+++ Back/App/routers/routerPrivate/userRouter.js
@@ -3,11 +3,17 @@ const express = require('express');
 /* --------------- Controllers ---------------*/
 const userControler = require('../../controllers/userController');
 
+/* --------------- Validations ---------------*/
+const { userBody } = require('../../validations/schemas');
+const validate = require('../../validations/validate');
+
 const router = express.Router();
 
 /* --------------- Routes ---------------*/
 router.get('/', userControler.detailUsers);
 // (\\d+) expression régulière qui definit une contrainte sur l'id.
 router.get('/:id(\\d+)', userControler.getOneUser);
+
+router.put('/:id(\\d+)', validate(userBody), userControler.modifyUser);
 
 module.exports = router;
