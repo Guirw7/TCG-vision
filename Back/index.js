67c81560cbaf562d203acf17b7baf4e105eeeaaa
Guirw7 <guillaume.barban27@gmail.com>
2023-06-15 15:04:36 +0200

    feat: set up sessions and config in the controller

--- Back/index.js
+++ Back/index.js
@@ -1,13 +1,18 @@
 // Load environnement variables
-const path = require("path");
+const path = require('path');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: path.join(__dirname, '.env')});
+dotenv.config({ path: path.join(__dirname, '.env') });
 
+// Require des modules
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+
+// Require des routers
 const routerPublic = require('./App/routers/routerPublic');
 const routerPrivate = require('./App/routers/routerPrivate');
+
 const logger = require('./App/log');
 
 // creation de l'application
@@ -16,7 +21,19 @@ const app = express();
 // Body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors('*'));
+
+// On configure les sessions
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  resave: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: parseInt(process.env.SESSION_MAX_AGE, 10),
+  },
+}));
 
 // Application routers
 app.use(routerPublic);
