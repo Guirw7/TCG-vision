84252671b52318a869796eca66d59bb33194294a
TalelJandoubi <talel.axis@gmail.com>
2023-06-16 14:57:43 +0200

    change idea of project

--- Back/App/routers/routerPublic/cardRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cardController = require('../../controllers/cardController');
-
-const router = express.Router();
-
-router.get('/', cardController.getAllCards);
-
-module.exports = router;
