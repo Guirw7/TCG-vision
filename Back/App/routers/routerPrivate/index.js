48237e42fc24a17895e8b5f04a5224e1d31cb5a2
Daoust-Jason <daoustjason2@gmail.com>
2023-06-20 11:05:34 +0200

    add feature create deck in db

--- Back/App/routers/routerPrivate/index.js
+++ Back/App/routers/routerPrivate/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const userRouter = require('./userRouter');
 const { error404 } = require('../../controllers/error');
 const errorHandler = require('../../helpers/errorHandler');
+const deckRouter = require('./deckRouter');
 
 const router = express.Router();
 
 router.use('/profil', userRouter);
+router.use('/deck', deckRouter);
 
 router.use(error404);
 router.use(errorHandler);
