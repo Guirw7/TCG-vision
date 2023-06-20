4644c958f9ed43afcc5ef3b191d16792fc205f5c
TalelJandoubi <talel.axis@gmail.com>
2023-06-20 11:19:25 +0200

    test with addCollectionInDb

--- Back/App/routers/routerPrivate/index.js
+++ Back/App/routers/routerPrivate/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const userRouter = require('./userRouter');
 const { error404 } = require('../../controllers/error');
 const errorHandler = require('../../helpers/errorHandler');
+const collectionRouter = require('./collectionRouter');
 
 const router = express.Router();
 
 router.use('/profil', userRouter);
+router.use('/collection', collectionRouter);
 
 router.use(error404);
 router.use(errorHandler);
