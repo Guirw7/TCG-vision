65f90dbe1628d9fd8c05c192e6c43a034ed8e4be
TalelJandoubi <talel.axis@gmail.com>
2023-06-21 15:32:51 +0200

    placement comments

--- Back/App/datamappers/collectionDataMapper.js
+++ Back/App/datamappers/collectionDataMapper.js
@@ -4,10 +4,10 @@
 // Recover the connection client
 const client = require('../db');
 
-/**
+const collectionDataMapper = {
+  /**
    * We build our userDataMapper object
    */
-const collectionDataMapper = {
   async addCollectionInDb(collection) {
     const preparedQuery = {
       text: 'INSERT INTO "collection"(collection_name,card_name,card_set,card_quantity,user_id, card_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', // introduction to the tables required for a collection
@@ -17,9 +17,13 @@ const collectionDataMapper = {
     const results = await client.query(preparedQuery);
     return results.rows;
   },
+  /**
+   * We build our userDataMapper object
+   */
   async getOneCollection(id) {
     const preparedQuery = {
-      text: 'SELECT * FROM collection WHERE "id" = $1', // allows you to view a specific collection
+      // allows you to view a specific collection
+      text: 'SELECT * FROM collection WHERE "id" = $1',
       values: [id],
     };
     // Execute a search request
