e529784062c107bb9ff49e8c18cbafef65755d94
TalelJandoubi <talel.axis@gmail.com>
2023-06-22 11:21:49 +0200

    request for get One Collection with user

--- Back/App/datamappers/collectionDataMapper.js
+++ Back/App/datamappers/collectionDataMapper.js
@@ -11,8 +11,8 @@ const collectionDataMapper = {
   async addCollectionInDb(collection) {
     const preparedQuery = {
       // introduction to the tables required for a collection
-      text: 'INSERT INTO "collection"(collection_name,card_name,card_set,card_quantity,user_id, card_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-      values: [collection.collection_name, collection.card_name, collection.card_set, collection.card_quantity, collection.user_id, collection.card_id],
+      text: 'INSERT INTO "collection"(collection_name,card_set,card_quantity,user_id,) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      values: [collection.collection_name, collection.card_set, collection.card_quantity, collection.user_id],
     };
     // Execute a search request
     const results = await client.query(preparedQuery);
@@ -24,7 +24,7 @@ const collectionDataMapper = {
   async getOneCollection(id) {
     const preparedQuery = {
       // allows you to view a specific collection
-      text: 'SELECT * FROM collection WHERE "id" = $1',
+      text: 'SELECT "user"."username", "collection".* FROM "collection" JOIN "user" ON "collection"."user_id" = "user"."id" WHERE "collection"."id" = $1',
       values: [id],
     };
     // Execute a search request
