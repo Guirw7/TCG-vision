815f1acb2a4f32e9e461c8646e6bf8425134f479
TalelJandoubi <talel.axis@gmail.com>
2023-06-22 16:42:14 +0200

    add second collection + select for all collection user

--- Back/App/routers/routerPrivate/collectionRouter.js
+++ Back/App/routers/routerPrivate/collectionRouter.js
@@ -9,6 +9,8 @@ const router = express.Router();
 /* --------------- Routes ---------------*/
 router.post('/', controllerHandler(collectionController.addCollectionInDb));
 router.get('/:id(\\d+)', controllerHandler(collectionController.getOneCollection));
+router.get('/collection/userId(\\d+)', controllerHandler(collectionController.getAllCollectionByUser));
 
 /* --------------- export of router ---------------*/
+
 module.exports = router;
