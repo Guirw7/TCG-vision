7a664cb02ef0f8d78d12d5fdee2dacf204eb05bf
Guirw7 <guillaume.barban27@gmail.com>
2023-06-26 12:42:11 +0200

    refactor: modify index.js routerPublic

--- Back/App/routers/routerPublic/index.js
+++ Back/App/routers/routerPublic/index.js
@@ -4,12 +4,7 @@ const deckRouter = require('./deckPublicRouter');
 
 const router = express.Router();
 
-<<<<<<< HEAD
-router.use('/user', userRouter);
-router.use('/decks', deckRouter);
-=======
 router.use('/public/user', userRouter);
 router.use('/public/decks', deckRouter);
->>>>>>> 14a1e955964c2458bf44430bbce8ecef532d7ea9
 
 module.exports = router;
