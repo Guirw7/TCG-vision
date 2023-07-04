24042446be361798e0e4d583b38d26c71c924059
Rafael Filipe <luis.rafael.filipe@gmail.com>
2023-07-04 10:44:06 +0200

    added config.json for the environment variables

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import { store } from './store';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './styles/index.scss';
 import App from './components/App';
 
 
-/*-- Render --*/
-
-/*-- Méthode 6.4 mais qui ne fonctionne pour pour la redirection --
-___________________________________________________________________________
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App/>
-    </Provider>
-  </React.StrictMode>
-);
-*/
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
