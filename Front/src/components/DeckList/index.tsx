6d05d8159838b42957bb4091d53833073df20698
Rafael Filipe <luis.rafael.filipe@gmail.com>
2023-07-07 00:31:09 +0200

    deck creator fixed

--- Front/src/components/DeckList/index.tsx
+++ Front/src/components/DeckList/index.tsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
 import { setUserDeck } from '../DeckEditorPage/userDeckSlice';
 import "./styles.scss";
 
-const DeckList = ({ deck }: { deck: any[] }) => {
+interface Card {
+  id: number;
+  name: string;
+  type: string;
+}
+
+const DeckList: React.FC = () => {
   const dispatch = useDispatch();
-  const [cardData, setCardData] = useState<any[]>([]);
-  const [filteredMonsters, setFilteredMonsters] = useState<any[]>([]);
-  const [filteredSpells, setFilteredSpells] = useState<any[]>([]);
-  const [filteredTraps, setFilteredTraps] = useState<any[]>([]);
-  const [filteredExtraDeck, setFilteredExtraDeck] = useState<any[]>([]);
+  const userDeck = useSelector((state: RootState) => state.userDeck);
+  const [cardData, setCardData] = useState<Card[]>([]);
 
   useEffect(() => {
     const fetchCardData = async () => {
-      const cardPromises = deck.map((id) =>
+      const cardPromises = userDeck.value.map((id) =>
         fetch(
           `https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${id}&language=fr`
         )
@@ -22,98 +26,103 @@ const DeckList = ({ deck }: { deck: any[] }) => {
       );
 
       const cardsData = await Promise.all(cardPromises);
-      const extractedCardData = cardsData.map((cardData) => cardData.data[0]); // Extraction des données réelles des cartes
+      const extractedCardData = cardsData.map((cardData) => cardData.data[0]);
       setCardData(extractedCardData);
     };
 
     fetchCardData();
-  }, [deck]);
-
-  useEffect(() => {
-    setCardData([]);
-  }, [deck]);
-
-  useEffect(() => {
-    const filteredMonsters = cardData.filter((card) =>
-      [
-        "Effect Monster",
-        "Flip Effect Monster",
-        "Flip Tuner Effect Monster",
-        "Gemini Monster",
-        "Normal Monster",
-        "Normal Tuner Monster",
-        "Pendulum Effect Monster",
-        "Pendulum Effect Ritual Monster",
-        "Pendulum Flip Effect Monster",
-        "Pendulum Normal Monster",
-        "Pendulum Tuner Effect Monster",
-        "Ritual Effect Monster",
-        "Ritual Monster",
-        "Spirit Monster",
-        "Toon Monster",
-        "Tuner Monster",
-        "Union Effect Monster",
-      ].includes(card.type)
-    );
-
-    const filteredSpells = cardData.filter((card) => card.type === "Spell Card");
-
-    const filteredTraps = cardData.filter((card) => card.type === "Trap Card");
-
-    const filteredExtraDeck = cardData.filter((card) =>
-      [
-        "Fusion Monster",
-        "Link Monster",
-        "Pendulum Effect Fusion Monster",
-        "Synchro Monster",
-        "Synchro Pendulum Effect Monster",
-        "Synchro Tuner Monster",
-        "XYZ Monster",
-        "XYZ Pendulum Effect Monster",
-      ].includes(card.type)
-    );
-
-    setFilteredMonsters(filteredMonsters);
-    setFilteredSpells(filteredSpells);
-    setFilteredTraps(filteredTraps);
-    setFilteredExtraDeck(filteredExtraDeck);
-  }, [cardData]);
+  }, [userDeck]);
 
   const handleCardClick = (cardId: number) => {
-    const cardIndex = deck.findIndex((id) => id === cardId);
+    const cardIndex = userDeck.value.findIndex((id) => id === cardId);
     if (cardIndex !== -1) {
-      const updatedDeck = [...deck];
+      const updatedDeck = [...userDeck.value];
       updatedDeck.splice(cardIndex, 1);
       dispatch(setUserDeck(updatedDeck));
     }
   };
 
+  const filteredMonsters = cardData.filter((card) =>
+    [
+      "Effect Monster",
+      "Flip Effect Monster",
+      "Flip Tuner Effect Monster",
+      "Gemini Monster",
+      "Normal Monster",
+      "Normal Tuner Monster",
+      "Pendulum Effect Monster",
+      "Pendulum Effect Ritual Monster",
+      "Pendulum Flip Effect Monster",
+      "Pendulum Normal Monster",
+      "Pendulum Tuner Effect Monster",
+      "Ritual Effect Monster",
+      "Ritual Monster",
+      "Spirit Monster",
+      "Toon Monster",
+      "Tuner Monster",
+      "Union Effect Monster",
+    ].includes(card.type)
+  );
+
+  const filteredSpells = cardData.filter((card) => card.type === "Spell Card");
+
+  const filteredTraps = cardData.filter((card) => card.type === "Trap Card");
+
+  const filteredExtraDeck = cardData.filter((card) =>
+    [
+      "Fusion Monster",
+      "Link Monster",
+      "Pendulum Effect Fusion Monster",
+      "Synchro Monster",
+      "Synchro Pendulum Effect Monster",
+      "Synchro Tuner Monster",
+      "XYZ Monster",
+      "XYZ Pendulum Effect Monster",
+    ].includes(card.type)
+  );
+
   return (
     <div>
       <h2>Monstres</h2>
-      {filteredMonsters.map((card, index) => (
-        <div className="decklist-item" key={index} onClick={() => handleCardClick(card.id)}>
+      {filteredMonsters.map((card) => (
+        <div
+          className="decklist-item"
+          key={card.id}
+          onClick={() => handleCardClick(card.id)}
+        >
           {card.name}
         </div>
       ))}
 
       <h2>Magies</h2>
-      {filteredSpells.map((card, index) => (
-        <div className="decklist-item" key={index} onClick={() => handleCardClick(card.id)}>
+      {filteredSpells.map((card) => (
+        <div
+          className="decklist-item"
+          key={card.id}
+          onClick={() => handleCardClick(card.id)}
+        >
           {card.name}
         </div>
       ))}
 
       <h2>Pièges</h2>
-      {filteredTraps.map((card, index) => (
-        <div className="decklist-item" key={index} onClick={() => handleCardClick(card.id)}>
+      {filteredTraps.map((card) => (
+        <div
+          className="decklist-item"
+          key={card.id}
+          onClick={() => handleCardClick(card.id)}
+        >
           {card.name}
         </div>
       ))}
 
       <h2>Extra Deck</h2>
-      {filteredExtraDeck.map((card, index) => (
-        <div className="decklist-item" key={index} onClick={() => handleCardClick(card.id)}>
+      {filteredExtraDeck.map((card) => (
+        <div
+          className="decklist-item"
+          key={card.id}
+          onClick={() => handleCardClick(card.id)}
+        >
           {card.name}
         </div>
       ))}
