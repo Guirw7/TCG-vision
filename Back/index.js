887c01ba85e9d7f93e39c327acd27efa0cb38776
Daoust-Jason <daoustjason2@gmail.com>
2023-07-12 18:41:36 +0200

    refactor: Add method PATCH in cors

--- Back/index.js
+++ Back/index.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
-  methods: 'GET, POST, PUT, DELETE',
+  methods: 'GET, POST, PUT, PATCH, DELETE',
   allowedHeaders: 'Authorization, Content-Type',
 }));
 
