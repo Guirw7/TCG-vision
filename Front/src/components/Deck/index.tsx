793e1fdf4c168234a941047019a472c027489df4
Guirw7 <guillaume.barban27@gmail.com>
2023-07-12 13:58:27 +0200

    feat: Added function isLoggedIn for create and see deck

--- Front/src/components/Deck/index.tsx
+++ Front/src/components/Deck/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { openModal } from '../LibraryModal/librarySlice';
 import { useNavigate } from 'react-router-dom';
@@ -6,45 +6,49 @@ import { useNavigate } from 'react-router-dom';
 import { axiosRequest } from '../../utils/axiosRequest';
 import { getIDFromToken } from '../../utils/getIDFromToken';
 import LibraryModal from '../LibraryModal';
-import DeckList from '../DeckList';
-import DeckDisplayer from '../DeckDisplayer';
-import './styles.scss';
 import SingleDeck from '../SingleDeck';
+import './styles.scss';
 
-export default function Deck () {
+export default function Deck() {
   const modal = useSelector((state: any) => state.libraryModal.value);
   const dispatch = useDispatch();
   const [decks, setDecks] = useState([]);
   const [request, setRequest] = useState<string>('');
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const jwt = sessionStorage.getItem('jwt');
+    setIsLoggedIn(jwt !== null);
+  }, []);
 
   const getAllDecks = async () => {
     setRequest('');
     axiosRequest('get', 'https://daoust-jason-server.eddi.cloud/public/decks')
-    .then(data => {
-      console.log(data);
-      setDecks(data);
-    })
-    .catch(error => {
-      console.log('Erreur lors de la requête', error);
-    });
+      .then((data) => {
+        console.log(data);
+        setDecks(data);
+      })
+      .catch((error) => {
+        console.log('Erreur lors de la requête', error);
+      });
   };
 
   const getUserDecks = () => {
     setRequest('');
-      const id = getIDFromToken();
-      console.log(id);
-      const url = `https://daoust-jason-server.eddi.cloud/private/deck/${id}`;
-      axiosRequest('get', url, {
-        'headers': {
-          'Authorization': `Bearer ${sessionStorage.getItem('jwt')}`
-        },
-      })
-      .then(data => {
+    const id = getIDFromToken();
+    console.log(id);
+    const url = `https://daoust-jason-server.eddi.cloud/private/deck/${id}`;
+    axiosRequest('get', url, {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('jwt')}`,
+      },
+    })
+      .then((data) => {
         console.log(data);
         setDecks(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log('Erreur lors de la requête', error);
       });
   };
@@ -53,15 +57,15 @@ export default function Deck () {
     event.preventDefault();
     const value = event.target.value;
     setRequest(value);
-  }
+  };
 
   useEffect(() => {
     if (request === 'getAllDecks') {
       getAllDecks();
-    };
+    }
     if (request === 'getUserDecks') {
       getUserDecks();
-    };
+    }
     if (!request) {
       return;
     }
@@ -74,7 +78,7 @@ export default function Deck () {
   const testDeCo = async () => {
     axiosRequest('get', 'https://daoust-jason-server.eddi.cloud/private/profil', {
       headers: {
-        'Authorization': `Bearer ${sessionStorage.getItem('jwt')}`
+        Authorization: `Bearer ${sessionStorage.getItem('jwt')}`,
       },
     });
   };
@@ -82,42 +86,37 @@ export default function Deck () {
   const getDecks = async () => {
     axiosRequest('get', 'https://daoust-jason-server.eddi.cloud/private/decks/1', {
       headers: {
-        'Authorization': `Bearer ${sessionStorage.getItem('jwt')}`
+        Authorization: `Bearer ${sessionStorage.getItem('jwt')}`,
       },
     });
   };
 
   return (
-    <div className='deck-container'>
-      <div className='deck-container-background'>
-        <h1 className='page-title'>Decks</h1>
-      {
-        (modal) && (
-            <LibraryModal />
-        )
-      }
-        <div className='decks-body'>
-
-          <div className='decks-actions'>
-
-            <button onClick={() => navigate('/deck-creator')}>Créer un deck</button>
-            <button onClick={userRequest} value='getAllDecks'>Tous les decks de la communauté</button>
-            <button onClick={userRequest} value='getUserDecks'>Vos decks</button>
-          </div >
-          <div className='decks-display'>
-
+    <div className="deck-container">
+      <div className="deck-container-background">
+        <h1 className="page-title">Decks</h1>
+        {modal && <LibraryModal />}
+        <div className="decks-body">
+          <div className="decks-actions">
+            {isLoggedIn && (
+              <>
+              <button onClick={() => navigate('/deck-creator')}>Créer un deck</button>
+            <button onClick={userRequest} value="getUserDecks">
+              Vos decks
+            </button>
+              </>
+            )}
+            <button onClick={userRequest} value="getAllDecks">
+              Tous les decks de la communauté
+            </button>
+          </div>
+          <div className="decks-display">
             {decks.map((deck: any) => {
-              return (
-                <SingleDeck key={deck.id} deck={deck}/>
-                )
-              })}
+              return <SingleDeck key={deck.id} deck={deck} />;
+            })}
           </div>
-          </div>
+        </div>
       </div>
     </div>
-  )
-};
-
-
-
-
+  );
+}
