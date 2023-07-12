220e51e6cce676e5ad53824f2f239983bcde3743
elFilipeLu <luis.rafael.filipe@gmail.com>
2023-07-12 11:28:46 +0200

    add card to collection functionnal

--- Front/src/components/CardModal/index.tsx
+++ Front/src/components/CardModal/index.tsx
@@ -6,6 +6,7 @@ import { openCardAdditionModal } from '../CardAdditionModal/cardAdditionSlice';
 import { axiosRequest } from '../../utils/axiosRequest';
 import { addCardToDeck } from '../../utils/addCardToLibrary';
 import CardAdditionModal from '../CardAdditionModal';
+import { getIDFromToken } from '../../utils/getIDFromToken';
 import './styles.scss';
 
 
@@ -23,10 +24,46 @@ export default function CardModal() {
   const [cardImage, setCardImage] = useState<any>(null);
   const [counter, setCounter] = useState<number>(1);
   const cardID = useSelector((state: any) => state.cardModal.element);
-  const [selectedExtension, setSelectedExtension] = useState<string>("");
+  const [selectedExtension, setSelectedExtension] = useState<string[]>([]);
   const [extensionList, setExtensionList] = useState<ExtensionProps[]>([]);
   const cardAdditionModal = useSelector((state: any) => state.cardAdditionModal.value);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const [collections, setCollections] = useState<any>([]);
+  const [selectedCollection, setSelectedCollection] = useState<any>(null);
   
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('jwt');
+    if (token) {
+      setIsConnected(true);
+    } else {
+      setIsConnected(false);
+    };
+  }), [];
+
+  useEffect(() => {
+    if (collections && collections.length > 0) {
+      setSelectedCollection(collections[0].id); // Set the default selected deck to the first deck in the list
+    }
+  }, [collections]);
+
+  useEffect(() => {
+    const id = getIDFromToken();
+    const url = `https://daoust-jason-server.eddi.cloud/private/collection/collection/${id}`;
+    axiosRequest('get', url, {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('jwt')}`
+      }
+    })
+    .then(data => {
+      setCollections(data);
+
+    })
+    .catch(error => {
+      console.log('Erreur lors de la requête', error);
+    });
+  }, []);
+
   const increment = (event: any) => {
     event?.preventDefault();
     setCounter(counter + 1);
@@ -52,17 +89,18 @@ export default function CardModal() {
     getCardDatabyExtension(event.target.value);
   };
 
+  const collectionOptionHandler = (event: any) => {
+    event.preventDefault();
+    setSelectedCollection(event.target.value)
+    
+  };
+
   const getCardDatabyExtension = async (selectedExtension: string) => {
     const cardExtensions = cardData.card_sets;
     const filteredExtensions = cardExtensions.filter((extension: ExtensionProps) => {
       return extension.set_code === selectedExtension;
     });
     setExtensionList(filteredExtensions);
-  };
-
-  const openDeckModal = (event: any) => {
-    event.preventDefault();
-    dispatch(openCardAdditionModal());
   };
 
   useEffect(() => {
@@ -76,6 +114,38 @@ export default function CardModal() {
       console.log('Erreur lors de la requête', error);
     });
   }, [cardID]);
+
+  const addCardToCollection = (event: any) => {
+    event.preventDefault();
+    let array = [selectedExtension];
+    console.log(parseInt(selectedCollection), counter, array);
+    for (let i = 0; i < counter; i++) {
+      const id = getIDFromToken();
+      const url = `https://daoust-jason-server.eddi.cloud/private/collection/${parseInt(selectedCollection)}`;
+      axiosRequest('put', url, {
+        data: {
+          set_code: array,
+          user_id: id
+          },
+          headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${sessionStorage.getItem('jwt')}`
+          },
+      })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log('Erreur lors de la requête', error);
+        });
+        };
+  }
+  
+
+  
+
+
+
 
   return (
     cardData && (
@@ -136,13 +206,14 @@ export default function CardModal() {
               <option value="">Sélectionnez une extension</option>
               {cardData.card_sets.map((extension: any, index: number) => {
                 return (
-                  <option key= {index} value={extension.set_code}>{extension.set_name}</option>
+                  <option key= {index} value={extension.set_code}>{extension.set_name}---{extension.set_code}</option>
                 )
               })}
             </select>
           </section>
 
-          {/* <div className='card-modal-buttons'>
+              {isConnected && (
+          <div className='card-modal-buttons'>
             <div>
               <section className='card-modal-quantity'>
                 <button onClick={decrement} className='card-modal-quantity-decrement'>-</button>
@@ -152,10 +223,20 @@ export default function CardModal() {
                 <button onClick={increment} className='card-modal-quantity-increment'>+</button>
               </section>
             </div>
-            <div className='card-modal-submit-buttons'>
-              <button type='submit' onClick={openDeckModal} className='card-modal-submit-button-deck'>Ajouter au Deck</button>
+            <div>
+              <select onChange={collectionOptionHandler} className='card-modal-extension-select'>
+                {collections.map((collection: any, index: number) => {
+                  return (
+                    <option key= {index} value={collection.id}>{collection.collection_name}</option>
+                  )
+                })}
+              </select>
             </div>
-          </div> */}
+            <div className='card-modal-submit-buttons'>
+              <button onClick={addCardToCollection} type='submit' className='card-modal-submit-button-deck'>Ajouter a la collection</button>
+            </div>
+          </div>
+              )}
         </form>
         </article>
         {
